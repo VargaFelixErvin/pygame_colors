1c14d2fd15445a951dd4568b16917e4860e2a7b8
VargaFelixErvin <130917917+VargaFelixErvin@users.noreply.github.com>
2023-12-27 16:50:24 +0100

    Add files via upload

---
+++ pygame alapok.py
@@ -0,0 +1,36 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+RED = (0, 255, 0)
+GREEN = (255, 0, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 300))
+pygame.display.set_caption('colors')
+
+background_color = WHITE
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_g:
+                background_color = RED
+            elif event.key == pygame.K_r:
+                background_color =  GREEN
+            elif event.key == pygame.K_w:
+                background_color =  WHITE
+
+
+
+
+
+
+
+    screen.fill(background_color)
+    pygame.display.update()
+
+pygame.quit()
